8d13af27e0c41d12d2ae7ba692c5546942d8db6f
gzoritchak <g.zoritchak@gmail.com>
2017-09-22 13:10:12 +0200

    Initial commit.

---
+++ src/main/kotlin/io/gzk/MainCode.kt
@@ -0,0 +1,4 @@
+package io.gzk
+
+
+fun hello() = "Hello Kotlin js"
---
+++ src/test/kotlin/io/gzk/TestMainCode.kt
@@ -0,0 +1,15 @@
+package io.gzk
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+
+class TestMainCode {
+
+    @Test
+    fun testHello() {
+        assertEquals("Hello Kotlin js", hello())
+    }
+
+}
+
